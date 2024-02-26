5c02b3e750211ed1b471c43f91ebda0ae9e25d52
Mihail20052005 <108936552+Mihail20052005@users.noreply.github.com>
2024-02-26 18:47:03 +0300

    add: main file and upd: combination.py

--- comb/combination.py
+++ comb/combination.py
@@ -134,30 +134,4 @@ class ArrangeWithoutRep(CombinatorialScheme):
 
 
 
-def main():
-    """Главная функция для выполнения программы."""
-    schemes = [
-        SumRule("Правило суммы"),
-        ProductRule("Правило произведения"),
-        PermWithRep("Перестановки с повторениями"),
-        PermWithoutRep("Перестановки без повторений"),
-        CombWithRep("Сочетания с повторениями"),
-        CombWithoutRep("Сочетания без повторений"),
-        ArrangeWithRep("Размещения с повторениями"),
-        ArrangeWithoutRep("Размещения без повторений")
-    ]
 
-    print("Выберите комбинаторную схему:")
-    for i, scheme in enumerate(schemes, start=1):
-        print(f"{i}. {scheme.name}")
-
-    choice = int(input("Введите номер схемы: "))
-
-    if choice >= 1 and choice <= len(schemes):
-        schemes[choice - 1].execute()
-    else:
-        raise Exception("Вы вышли за границы, попробуйте еще раз")
-
-
-if __name__ == "__main__":
-    main()
